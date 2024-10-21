a05811c75334cbdbabc65820e591aabd16048a59
vladbakharev <vlad97b14@gmail.com>
2024-10-21 23:41:15 +0300

    Card, FAB, AddWord Activity

--- app/src/main/java/com/vladbakharev/otzarivrit/MainActivity.kt
+++ app/src/main/java/com/vladbakharev/otzarivrit/MainActivity.kt
@@ -4,13 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vladbakharev.otzarivrit.ui.theme.OtzarIvritTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +41,177 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OtzarIvritTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Otzar Ivrit",
-                        modifier = Modifier.padding(innerPadding)
+
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OtzarIvritApp(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        stringResource(R.string.app_name)
                     )
-                }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(Icons.Default.Add, contentDescription = stringResource(R.string.fab_add))
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+
+        }
+    }
+}
+
+@Composable
+fun OtzarIvritCard(modifier: Modifier = Modifier) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(0.5f)
+            ) {
+                Text(
+                    modifier = Modifier,
+                    text = "Transcription",
+                    style = MaterialTheme.typography.titleSmall,
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .weight(0.5f),
+            ) {
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.End), text = "Word",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.End
+                )
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.End),
+                    text = "Translation",
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.End
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun OtzarIvritAddWord(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(start = 16.dp),
+            text = "Word",
+            style = MaterialTheme.typography.titleLarge,
+        )
+        TextField(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            value = "",
+            onValueChange = {},
+            label = { Text("Word") }
+        )
+        Text(
+            modifier = Modifier
+                .padding(start = 16.dp),
+            text = "Translation",
+            style = MaterialTheme.typography.titleLarge,
+        )
+        TextField(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            value = "",
+            onValueChange = {},
+            label = { Text("Translation") }
+        )
+        Text(
+            modifier = Modifier
+                .padding(start = 16.dp),
+            text = "Transcription",
+            style = MaterialTheme.typography.titleLarge,
+        )
+        TextField(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            value = "",
+            onValueChange = {},
+            label = { Text("Transcription") }
+        )
+        Button(
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.End),
+            onClick = {},
+
+            ) {
+            Text("Add")
+        }
+
+    }
 }
+
+
+@Preview(showBackground = true)
+@Composable
+fun OtzarIvritAddWordPreview() {
+    OtzarIvritTheme {
+        OtzarIvritAddWord()
+    }
+}
+
+@Preview
+@Composable
+fun OtzarIvritCardPreview() {
+    OtzarIvritTheme {
+        OtzarIvritCard()
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     OtzarIvritTheme {
-        Greeting("Otzar Ivrit")
+        OtzarIvritApp(modifier = Modifier)
+
     }
 }
