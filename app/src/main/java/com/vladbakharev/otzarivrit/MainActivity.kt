fd18625d94d0f7548f5c68b8556201821609176d
vladbakharev <vlad97b14@gmail.com>
2024-11-08 22:42:13 +0200

    BottomSheetScaffold

--- app/src/main/java/com/vladbakharev/otzarivrit/MainActivity.kt
+++ app/src/main/java/com/vladbakharev/otzarivrit/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.vladbakharev.otzarivrit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,17 +20,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -39,6 +50,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -48,6 +60,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -151,13 +165,16 @@ fun WordsList(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun WordCard(
     modifier: Modifier = Modifier,
     word: Word
 ) {
-    val haptics = LocalHapticFeedback.current
+//    val haptics = LocalHapticFeedback.current
     var toggleButtonChecked by remember { mutableStateOf(false) }
+    var isBottomSheetVisible by remember { mutableStateOf(false) }
+
     Card(
         modifier = modifier
             .fillMaxWidth()
@@ -168,12 +185,13 @@ fun WordCard(
                 bottom = 8.dp
             )
             .height(120.dp)
-        /*.combinedClickable(
-            onLongClick = {
-                haptics.performHapticFeedback(HapticFeedbackType.LongPress)
-
-            }
-        )*/,
+            .combinedClickable(
+                onLongClick = {
+                    isBottomSheetVisible = true
+                    Log.d("TAG", "WordCard: ${word.word}")
+                },
+                onClick = {}
+            ),
         shape = RoundedCornerShape(32.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
@@ -239,6 +257,53 @@ fun WordCard(
                 )
             }
         }
+    }
+    if (isBottomSheetVisible) {
+        CardBottomSheetScaffold(
+            modifier = modifier,
+            onDismissRequest = { isBottomSheetVisible = false })
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardBottomSheetScaffold(
+    modifier: Modifier = Modifier,
+    onDismissRequest: () -> Unit
+) {
+    val scaffoldState = rememberBottomSheetScaffoldState()
+
+    BottomSheetScaffold(
+        modifier = modifier,
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 128.dp,
+        sheetSwipeEnabled = false,
+        sheetContent = {
+            Column {
+                Row(
+                    modifier = modifier
+//                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Icon(
+                        modifier = modifier
+                            .padding(16.dp),
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null
+                    )
+                    Text(
+                        modifier = modifier
+                            .padding(16.dp),
+                        text = stringResource(R.string.delete)
+                    )
+
+                }
+            }
+        }
+    ) {
+
     }
 }
 
@@ -652,6 +717,20 @@ fun BottomNavigationBarPreview() {
         NavigationBar(navController = NavController(MainActivity()))
     }
 }
+
+@Preview
+@Composable
+fun CardBottomSheetScaffoldPreview() {
+    OtzarIvritTheme {
+        CardBottomSheetScaffold(
+            modifier = Modifier,
+            onDismissRequest = {}
+        )
+    }
+}
+
+
+
 
 
 
